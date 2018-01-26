bf6b0ef81804783eff759fc3293cade3395dadce
akhil <akhil191095@icloud.com>
2018-01-27 00:48:23 +0530

    My first python project

---
+++ main.py
@@ -0,0 +1,8 @@
+print "Hello"
+print "What is your name"
+name=raw_input ()
+print "Are you a Mr. or Ms."
+sex=raw_input ()
+print "What is your age"
+age=raw_input ()
+print "Welcome"+" "+sex+" "+name+" "+"I'd like to know a little bit more about you"
